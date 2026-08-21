4321d24e42656dfd24efbd4d4001f578c71e5a6c
thedetaildoctor775-rgb <thedetaildoctor775@gmail.com>
2026-08-21 01:49:50 -0700

    Inject Sierra Dispatch phone messenger

--- api/legacy.js
+++ api/legacy.js
@@ -5,9 +5,10 @@ export default function handler(req,res){
   try{
     const file=path.join(process.cwd(),'legacy.html');
     let html=fs.readFileSync(file,'utf8');
-    if(!html.includes('market-boost.js')){
-      html=html.replace(/<\/body>/i,'<script src="/market-boost.js"></script><script src="/market-cap-fix.js"></script></body>');
-    }
+    const scripts=[];
+    if(!html.includes('market-boost.js')) scripts.push('<script src="/market-boost.js"></script><script src="/market-cap-fix.js"></script>');
+    if(!html.includes('dispatch-phone.js')) scripts.push('<script src="/dispatch-phone.js"></script>');
+    if(scripts.length) html=html.replace(/<\/body>/i,scripts.join('')+'</body>');
     res.setHeader('Content-Type','text/html; charset=utf-8');
     res.setHeader('Cache-Control','no-store');
     return res.status(200).send(html);
