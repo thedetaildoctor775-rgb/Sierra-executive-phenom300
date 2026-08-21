117ee3ea21e0d090f3b65b5d4070b12e5d0f8d93
thedetaildoctor775-rgb <thedetaildoctor775@gmail.com>
2026-08-21 07:13:54 -0700

    Inject Dispatch standby guard

--- api/legacy.js
+++ api/legacy.js
@@ -8,6 +8,7 @@ export default function handler(req,res){
     const scripts=[];
     if(!html.includes('market-boost.js')) scripts.push('<script src="/market-boost.js"></script><script src="/market-cap-fix.js"></script>');
     if(!html.includes('dispatch-phone.js')) scripts.push('<script src="/dispatch-phone.js"></script>');
+    if(!html.includes('dispatch-standby-fix.js')) scripts.push('<script src="/dispatch-standby-fix.js"></script>');
     if(scripts.length) html=html.replace(/<\/body>/i,scripts.join('')+'</body>');
     res.setHeader('Content-Type','text/html; charset=utf-8');
     res.setHeader('Cache-Control','no-store');
